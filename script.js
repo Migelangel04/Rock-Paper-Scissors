b2ed6e8f0e56812e45402fdcbbe5f39101d154b5
Migelangel Carrillo <migelangelcarrillo2@gmail.com>
2023-08-11 13:11:43 -0700

    This version of the game is used using JavaScript console. Usability will be added in a later update to reflect a web game (i.e. with proper HTML and CSS)

---
+++ script.js
@@ -0,0 +1,107 @@
+function getComputerChoice()
+{
+   let choice = Math.floor(Math.random() * 3) + 1; 
+
+   if (choice === 1)
+   {
+    return "rock";
+   }
+   else if (choice === 2)
+   {
+    return "paper";
+   }
+   else
+   {
+    return "scissors";
+   }
+}
+
+function playAround(playerChoice, computerChoice)
+{
+    if (playerChoice === "rock")
+    {
+        if (computerChoice === "rock")
+        {
+            return "Tied Game! Go again.";
+        }
+        else if (computerChoice === "paper")
+        {
+            return "You Lose! Paper beats Rock.";
+        }
+        else 
+        {
+            return "You Win! Congrats";
+        }
+    }
+
+    else if (playerChoice === "paper")
+    {
+        if (computerChoice === "paper")
+        {
+            return "Tied Game! Go again.";
+        }
+        else if (computerChoice === "scissors")
+        {
+            return "You Lose! Scissors beats Paper.";
+        }
+        else 
+        {
+            return "You Win! Congrats";
+        }
+    }
+
+    else  // Scissors === playerChoice
+    {
+        if (computerChoice === "scissors")
+        {
+            return "Tied Game! Go again.";
+        }
+        else if (computerChoice === "rock")
+        {
+            return "You Lose! Rock beats Scissors.";
+        }
+        else 
+        {
+            return "You Win! Congrats";
+        }
+    }
+
+}
+
+function game()
+{
+    console.log("This game is the best to 3");
+    let userCounter = 0;
+    let computerCounter = 0;
+
+    while(userCounter < 3 && computerCounter < 3)
+    {
+        let computerChoice = getComputerChoice();
+        let playerChoice = prompt("What is your choice?");
+        let resultOfRound = playAround(playerChoice.toLowerCase(), computerChoice);
+
+        if (resultOfRound.includes("Lose"))
+        {
+            computerCounter++;
+            console.log(resultOfRound);
+        }
+        else if (resultOfRound.includes("Win"))
+        {
+            userCounter++;
+            console.log(resultOfRound);
+        }
+        else 
+        {
+            console.log(resultOfRound);
+        }
+    }
+
+    if (userCounter > computerCounter)
+    {
+        console.log("You have won the game! Score: " + userCounter + "-" + computerCounter);
+    }
+    else
+    {
+        console.log("You have loss :(. Score: " + computerCounter + "-" + userCounter);
+    }
+}
